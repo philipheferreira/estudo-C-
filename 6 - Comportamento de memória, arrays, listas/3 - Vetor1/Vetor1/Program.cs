d2890a197e63e413285db73a91795f73d914bc24
philipheferreira <philipheferreira@gmail.com>
2024-01-15 15:35:27 -0300

    criando 2 paginas

--- 6 - Comportamento de memória, arrays, listas/3 - Vetor1/Vetor1/Program.cs
+++ 6 - Comportamento de memória, arrays, listas/3 - Vetor1/Vetor1/Program.cs
@@ -9,6 +9,19 @@ namespace Treino{
 
             double[] vetor = new double[n];
 
+            for (int i = 0; i< n; i++){
+                vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++){
+                sum += vetor[i];
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
         }
 
 
