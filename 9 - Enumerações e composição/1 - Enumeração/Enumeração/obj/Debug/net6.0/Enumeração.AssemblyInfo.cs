afc436eed1608a5cc03d6af779dbd46bdc5632fe
philipheferreira <philipheferreira@gmail.com>
2024-01-17 13:26:10 -0300

    update: herança

--- 9 - Enumerações e composição/1 - Enumeração/Enumeração/obj/Debug/net6.0/Enumeração.AssemblyInfo.cs
+++ 9 - Enumerações e composição/1 - Enumeração/Enumeração/obj/Debug/net6.0/Enumeração.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enumeração")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9deb976e95a881f57a4f22c9bfda063b47a5bf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enumeração")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enumeração")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
