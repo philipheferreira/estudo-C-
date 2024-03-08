667b8f9ef06689ba128ea92f8f93ab36bbc8f70d
philipheferreira <philipheferreira@gmail.com>
2024-03-07 23:49:51 -0300

    update: heranca and enum

---
+++ 10 - Heranca e polimorfismo/2 - Upcasting e downcasting/updown/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using updown.Entities;
+
+namespace Teste
+{
+
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Treino geral");
+        }
+
+
+    }
+
+
+}
