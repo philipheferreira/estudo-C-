9c8ef5f901e2b937a3d0ede12943a775cca4269d
philipheferreira <philipheferreira@gmail.com>
2024-03-14 11:52:17 -0300

    update: simple inerence, polymorfism and properties

---
+++ 5 - Construtores, palavras/InerenciaSimples/InerenciaSimples/Program.cs
@@ -0,0 +1,28 @@
+﻿class Veiculo{
+
+    public string montadora = "Ford";
+
+    public void buzina(){
+        Console.WriteLine("Biii, biii!");
+    }
+
+}
+
+
+class Carro : Veiculo{
+    public string modeloNome = "Mustang";
+} 
+
+class Programa{ 
+
+    static void Main(string[] args){ 
+    
+        Carro meuCarro = new Carro();
+
+        meuCarro.buzina();
+
+        Console.WriteLine(meuCarro.montadora + " " + meuCarro.modeloNome);
+
+    }
+
+}
---
+++ 5 - Construtores, palavras/polimorfismosimples/polimorfismosimples/Program.cs
@@ -0,0 +1,30 @@
+﻿class Animal{ //classe base (pai)
+
+    public void somAnimal(){
+        Console.WriteLine("O animal faz um som");
+    }
+
+}
+
+class Porco : Animal{ 
+    public void somAnimal(){
+        Console.WriteLine("O porco diz: Wee wee");
+    }
+}
+
+class Cachorro : Animal{ 
+    public void somAnimal() { Console.WriteLine("O cachorro diz: au au");
+    }
+}
+
+class Programa{
+    static void Main(string[] args){ 
+        Animal meuAnimal = new Animal();
+        Animal meuPorco = new Porco();
+        Animal meuCachorro = new Cachorro();
+
+        meuAnimal.somAnimal();
+        meuPorco.somAnimal();
+        meuCachorro.somAnimal();
+    }
+}
