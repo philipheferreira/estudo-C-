10213c9bf5bd5fa4dc70e99b2a00994ae84c425d
philipheferreira <philipheferreira@gmail.com>
2024-04-08 17:03:13 -0300

    update: things i forget

--- 10 - Heranca e polimorfismo/2 - Upcasting e downcasting/updown/Entities/BusinessAccount.cs
+++ 10 - Heranca e polimorfismo/2 - Upcasting e downcasting/updown/Entities/BusinessAccount.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace updown.Entities
-{
+namespace updown.Entities{
     internal class BusinessAccount : Account{
 
         public double LoanLimit { get; set; }
 
-        public BusinessAccount() { }
+        public BusinessAccount(int numero, string holder, double balanco, double loanLimit) : base(numero, holder, balanco){
+            LoanLimit = loanLimit;
+        }
 
         public void Emprestimo(double valor){ 
             if (valor < LoanLimit){
