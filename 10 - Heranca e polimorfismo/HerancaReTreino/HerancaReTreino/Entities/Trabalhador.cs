590704f024a4607d894731c2c03e8ddcc17fdc59
philipheferreira <philipheferreira@gmail.com>
2024-07-15 16:34:44 -0300

    Create: heranca revision

---
+++ 10 - Heranca e polimorfismo/HerancaReTreino/HerancaReTreino/Entities/Trabalhador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaReTreino.Entities{
+    internal class Trabalhador : Pessoa{
+        public double HorarioDeTrabalho { get; set; }
+
+        public Trabalhador() { }
+
+        public Trabalhador(string nome, int idade, double totalDePassos) : base(nome, idade, totalDePassos){
+            HorarioDeTrabalho = HorarioDeTrabalho;
+        }
+
+        public void Salto(double duploPasso){ 
+            TotalDePassos = TotalDePassos + (duploPasso * 2);
+        }
+    }
+}
