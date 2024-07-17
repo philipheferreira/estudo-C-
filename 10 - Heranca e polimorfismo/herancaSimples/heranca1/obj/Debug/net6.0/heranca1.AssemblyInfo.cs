860e12eefcf33f901902239419f8d85b08c0b4c8
philipheferreira <philipheferreira@gmail.com>
2024-07-17 14:56:59 -0300

    Feat: add project heranca

--- 10 - Heranca e polimorfismo/herancaSimples/heranca1/obj/Debug/net6.0/heranca1.AssemblyInfo.cs
+++ 10 - Heranca e polimorfismo/herancaSimples/heranca1/obj/Debug/net6.0/heranca1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("heranca1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590704f024a4607d894731c2c03e8ddcc17fdc59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b64dd37d568980fe1c37fc2bc6db6ab371266c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("heranca1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("heranca1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
