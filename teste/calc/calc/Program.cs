8d6e511fce7e7ffb24023dff1252c88476f74f27
philipheferreira <philipheferreira@gmail.com>
2024-10-09 15:55:46 -0300

    soma, div, sub, mult operations calc

--- teste/calc/calc/Program.cs
+++ teste/calc/calc/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             float valor1, valor2;
+            float soma, sub, div, mult;
 
             Console.WriteLine("Digite o valor da primeira variavel: ");
             valor1 = float.Parse(Console.ReadLine());
@@ -13,6 +14,16 @@
             valor2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine($" O valor inserido para a primeira variavel é {valor1} e o segundo é {valor2} ");
+            
+            soma = valor1 + valor2;
+
+            sub = valor1 - valor2;
+
+            div = valor1 / valor2;
+
+            mult = valor1 * valor2;
+
+            Console.WriteLine("Operações:\nSoma: " + soma + "\nSubtração: " + sub + "\nDivisão: " + div + "\nMultiplicação: " + mult);
 
         }
     }
