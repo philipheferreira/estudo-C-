d07648daf3b94a9577c3cc28289015d83feecdc8
philipheferreira <philipheferreira@gmail.com>
2024-10-19 11:31:20 -0300

    Create: simple calc

---
+++ teste/Calculadoras/CalculadoraSimples/CalculadoraSimples/Program.cs
@@ -0,0 +1,17 @@
+﻿//Calculadora feita com as variaveis e metodos criadas em apenas uma classe principal
+
+namespace CalculadoraSimples{
+    class Program{
+        static void Main(string[] args){
+
+            int menu;
+
+            float numero1, numero2;
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
