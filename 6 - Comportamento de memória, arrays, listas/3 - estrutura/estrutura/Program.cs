34a22136cebea525be380961859f61df70c932cc
philipheferreira <philipheferreira@gmail.com>
2024-11-13 10:11:47 -0300

    Feat: anotations class

--- 6 - Comportamento de memória, arrays, listas/3 - estrutura/estrutura/Program.cs
+++ 6 - Comportamento de memória, arrays, listas/3 - estrutura/estrutura/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+
+Structs são tipos valor: A linguagem C# possui também tipos valor, que são os 'structs'. Structs são CAIXAS e não ponteiros.
+
+*/
+
+
+using System;
 
 namespace estrutura { 
 class Programa{
--- 6 - Comportamento de memória, arrays, listas/3 - estrutura/estrutura/obj/Debug/net6.0/estrutura.AssemblyInfo.cs
+++ 6 - Comportamento de memória, arrays, listas/3 - estrutura/estrutura/obj/Debug/net6.0/estrutura.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("estrutura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d90924ebb01039043143c6d4fbcbfc57aed2019b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce06bd242459f4f022651e05b9166671435892d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("estrutura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("estrutura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
