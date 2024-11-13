e70041d02e5b5399ba395babe535e947109d4a8c
philipheferreira <philipheferreira@gmail.com>
2024-11-13 16:02:13 -0300

    Create: params cap

---
+++ 6 - Comportamento de memória, arrays, listas/7-modificadorParametrosParams/7-modificadorParametrosParams/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _7_modificadorParametrosParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int s1 = Calculadora.Sum( new int[] { 2, 3 });
+            int s2 = Calculadora.Sum( new int[] { 2, 4, 3 });
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
