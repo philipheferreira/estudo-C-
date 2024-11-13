19aabed2b3a45ccc684dc9ec44f4afc7cd176b74
philipheferreira <philipheferreira@gmail.com>
2024-11-13 16:23:28 -0300

    Create: ref out

---
+++ 6 - Comportamento de memória, arrays, listas/8-modificadoresdeParametrosrefeout/8-modificadoresdeParametrosrefeout/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+
+Modificador ref: Suponha que se queira uma calculadora com uma operação para triplicar
+o valor de um número passado como parâmetro
+
+
+*/
+
+namespace _8_modificadoresdeParametrosrefeout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
