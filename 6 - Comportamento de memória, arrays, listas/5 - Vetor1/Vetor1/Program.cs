557e5ca70a2fb39fddda35aea69e75e31bfffa1b
philipheferreira <philipheferreira@gmail.com>
2024-11-13 11:12:49 -0300

    Feat: Vetor Things

--- 6 - Comportamento de memória, arrays, listas/5 - Vetor1/Vetor1/Program.cs
+++ 6 - Comportamento de memória, arrays, listas/5 - Vetor1/Vetor1/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+
+    * Em programação, "vetor" é o nome dado a arranjos unidimensionais
+    * Arranjo é uma estrutura de dados:
+        - Homogênea (dados do mesmo tipo);
+        - Ordenada (Elementos acessados por meio de posições)
+        - Alocada de uma vez só, em um bloco contíguo de memória
+    
+    * Vantagens:
+        - Acesso imediato aos elementos pela sua posição;
+    * Desvantagens:
+        - Tamanho fixo;
+        - Dificuldade para se realizar inserções e deleções
+
+*/
+
+
+using System;
 using System.Globalization;
 
 namespace Treino{
