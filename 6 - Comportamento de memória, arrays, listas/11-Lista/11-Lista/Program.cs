f0c88488ce55a4d576a3aef7a76624d432fc4b35
philipheferreira <philipheferreira@gmail.com>
2024-11-14 17:29:38 -0300

    Feat: list comments and image

--- 6 - Comportamento de memória, arrays, listas/11-Lista/11-Lista/Program.cs
+++ 6 - Comportamento de memória, arrays, listas/11-Lista/11-Lista/Program.cs
@@ -1,4 +1,30 @@
-﻿namespace _11_Lista
+﻿/*
+
+Listas
+
+* Lista é uma estrutura de dados:
+    - Homogênea (dados do mesmo tipo)
+    - Ordenada (Elementos acessados por meio de posições)
+    - Inicia vazia, e seus elementos são alocados sob demanda
+    - Cada elemento ocupa um "nó" (ou nodo) da lista
+
+* Classe: List
+* NameSpace: System.Collections.Generic
+
+
+* Vantagens:
+    - Tamanho Variável;
+    - Facilidade para se realizar inserções e deleções
+
+* Desvantagens:
+    - Acesso Sequencial aos elementos
+
+
+*/
+
+
+
+namespace _11_Lista
 {
     internal class Program
     {
